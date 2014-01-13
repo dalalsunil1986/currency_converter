a2aec4af354959ad6a9e4fa201f2549b970c53b6
jcapillo <jcapillo@internetbrands.com>
2014-01-13 10:16:29 -0800

    connected to database

--- src/CurrencyConverter/CurrencyConverterBundle/Resources/public/js/page_controller.js
+++ src/CurrencyConverter/CurrencyConverterBundle/Resources/public/js/page_controller.js
@@ -6,4 +6,182 @@
  * Version: 1.0
  *
  */
+
+ConverterApp.config(['$httpProvider', function($httpProvider) {
+    var interceptor = ['$q', 'LoadingIndicatorHandler', function($q, LoadingIndicatorHandler) {
+        return function(promise) {
+            LoadingIndicatorHandler.enable();
+            
+            return promise.then(
+                function( response ) {
+                    LoadingIndicatorHandler.disable();
+                    
+                    return response;
+                },
+                function( response ) {
+                    LoadingIndicatorHandler.disable();
+                    
+                    // Reject the reponse so that angular isn't waiting for a response.
+                    return $q.reject( response );
+                }
+            );
+        };
+    }];
+    
+    $httpProvider.responseInterceptors.push(interceptor);
+    
+}]);
+ 
+
+ConverterApp.factory('LoadingIndicatorHandler', function()
+{
+   
+    var $element = $('#loading-indicator');
+    
+    return {
+        // Counters to keep track of how many requests are sent and to know
+        // when to hide the loading element.
+        enable_count: 0,
+        disable_count: 0,
+        
+        /**
+         * Fade the blocker in to block the screen.
+         *
+         * @return {void}
+         */
+        enable: function() {
+            this.enable_count++;
+            
+            if ( $element.length ) $element.show();
+        },
+        
+        /**
+         * Fade the blocker out to unblock the screen.
+         *
+         * @return {void}
+         */
+        disable: function() {
+            this.disable_count++;
+            
+            if ( this.enable_count == this.disable_count ) {
+                if ( $element.length ) $element.hide();
+            }
+        }
+    }
+});
+
+ConverterApp.factory('pageFactory', function($http) {
+    return {        
+        baseRequest : function(url) {          
+            return $http.get(url).then(function(result) {
+               return result.data;
+            });
+        }
+        
+    };
+});
+
+/**
+ * Used to restrict user for entering non-numeric inputs
+ * Used as client-max 
+ * @param {type} clientMax the name of directive
+ * 
+ * 
+ */
+ConverterApp.directive('amountConvert', function() {
+  return {
+    require: 'ngModel',
+    link: function (scope, element, attr, ngModelCtrl) {
+      function fromUser(text) {
+        if(!text)
+            return false;
+        var transformedInput = text.replace(/[^0-9.]/g, '');
+       
+        if(transformedInput !== text) {
+            ngModelCtrl.$setViewValue(transformedInput);
+            ngModelCtrl.$render();
+        }
+        return transformedInput;  // or return Number(transformedInput)
+      }
+      ngModelCtrl.$parsers.push(fromUser);
+    }
+  }; 
+});
+
+
+
+function page_controller($scope, $http){
+    
+    var currencies_url = '../web/api/load'; //api for retrieving currencies
+    
+    $scope.amount = null;
+    $scope.currency_code_input = null; //currency code to convert
+    $scope.currency_code_output = null; //currency code to output
+    $scope.currencies = [];
+    $scope.resultMessage = [];
+    
+    
+    //called on page load
+    $scope.init = function() {
+        console.log('Initializing.......');
+        retrieveCurrencies();
+    };
+    
+    $scope.getFlag = function(symbol){
+        var url = 'http://s.xe.com/v2/themes/xe/images/flags/big/'+symbol.toLowerCase()+'.png';
+        return "background-image:url("+url+");";
+    };
+    
+    $scope.getSymbol = function(symbol){
+        if(symbol !== undefined && symbol && symbol !== null){
+            return "( "+symbol+" )";
+        }
+        else{
+            return "";
+        }
+    };
+    
+    $scope.convert = function(){
+       console.log('Submitting.....'); 
+    };
+    
+    /**
+     * This do an ajax call and fill-up the dropdown list with currencies.
+     * 
+     * @returns {undefined}
+     */
+    var retrieveCurrencies = function(){
+        
+         $http.get(currencies_url).success(function(data) {
+            
+             if(!data.error){                  
+                    
+                    $scope.currencies = data.data;
+                    
+            }
+            else
+               //show the result container
+               handleResponse('error',data.error.message);          
+            
+           
+        }).error(function() {            
+           handleResponse('error','Something\'s wrong happened.');
+        });
+         
+    };
+    
+    
+    
+    
+    var handleResponse = function(type,message){
+         var response = new Array();
+         response.type = type;
+         var response_body = {responseMsg:message};
+         response.push(response_body);
+         $scope.resultMessage = response;
+         return false;
+     };
+     
+    
+}
  
